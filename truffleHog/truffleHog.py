b74aafb48296ab6feaf27f511f6bbb3c7d6dd268
Wouter Bloeyaert <someniak@hotmail.com>
2018-05-22 11:22:44 +0200

    Fixed branching issue

--- truffleHog/truffleHog.py
+++ truffleHog/truffleHog.py
@@ -258,6 +258,8 @@ def find_strings(git_url, since_commit=None, max_depth=1000000, printJson=False,
             if since_commit and since_commit_reached:
                 prev_commit = curr_commit
                 break
+            # Fix to set commit hash back one step
+            commit_hash = prev_commit
             # if not prev_commit, then curr_commit is the newest commit. And we have nothing to diff with.
             # But we will diff the first commit with NULL_TREE here to check the oldest code.
             # In this way, no commit will be missed.
